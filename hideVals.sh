0f0ba2e325dcb7dd746a13a0e81179dd81347743
Miles Young <mwyoung2@gmail.com>
2019-10-19 14:27:08 -0700

    Finished top side, still have room for more parts...

--- hideVals.sh
+++ hideVals.sh
@@ -5,4 +5,4 @@
 file="Adafruit PCB Reference Ruler.kicad_pcb"
 
 #sed -i '/fp_text reference [^J^T][^P]\s/s/$/ hide/g' "$file"
-sed -i '/fp_text reference [^J^T][^P]\s/s/F.SilkS/Dwgs.User/g' "$file"
+sed -i '/fp_text reference \s/s/F.SilkS/Dwgs.User/g' "$file"
