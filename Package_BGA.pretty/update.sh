3bb09575965bd5134bb665bdcd35fa74e1ca6abb
Miles Young <mwyoung2@gmail.com>
2019-10-22 15:21:57 -0700

    Finished pad placement for redesign, still have a few more changes to do

---
+++ Package_BGA.pretty/update.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#updates Package from KiCad
+package_file="Package_BGA.pretty"
+package_file_7z="$package_file.7z"
+kicad_url="https://kicad.github.io/download/footprints"
+
+if [[ -e "$package_file_7z" ]]; then
+    rm "$package_file_7z"
+fi
+
+wget  $kicad_url/$package_file_7z
+
+7z e $package_file_7z
